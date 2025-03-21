db83d183f588452c16d77a5041c2d226b9d947eb
Sumbal Naz <Codeq209@gmail.com>
2025-03-21 20:15:29 +0500

    Add files via upload

---
+++ Q-03/index.py
@@ -0,0 +1,7 @@
+# Write a Python program that takes a number from the user and checks whether it is even
+num = int(input("Enter a Number: "))
+if num % 2 == 0:
+    print(f"{num} is an Even number")
+else:
+    print(f"{num} is not an Even number")
+    
