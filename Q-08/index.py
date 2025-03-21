27789e66539cd2e4cee19a0319ecad8598cb9473
Sumbal Naz <Codeq209@gmail.com>
2025-03-21 20:16:46 +0500

    Add files via upload

---
+++ Q-08/index.py
@@ -0,0 +1,13 @@
+#1-Add two Numbers
+#Solution1 - Predefined variables
+num1 = 30
+num2 = 20
+sum = num1 + num2
+print("the sum of given two number is" , sum)
+
+#Solution2 - With User Inputs
+num1 = float(input('Enter a Number Here:'))
+num2 = float(input('Enter Another Number here:'))
+sum = num1 + num2
+print("The Sum of the provided two numbers is" ,sum)
+
