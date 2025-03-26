dd4368d273331499329e5e4c02f407caa26dfa96
Sumbal Naz <Codeq209@gmail.com>
2025-03-26 14:20:19 +0500

    Add files via upload

---
+++ Q-14/index.py
@@ -0,0 +1,4 @@
+#Take a number as input and check whether it is prime or not
+num = int(input("Enter a number: "))
+is_prime = num > 1 and all(num % i != 0 for i in range(2, int(num**0.5) + 1))
+print("Prime" if is_prime else "Not Prime")
