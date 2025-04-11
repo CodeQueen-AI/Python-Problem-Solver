7e0f0b5fe424457c28f2f1b00e98cbb235e6770e
Sumbal Naz <Codeq209@gmail.com>
2025-04-11 09:09:29 +0500

    Update index.py

--- Q-35/index.py
+++ Q-35/index.py
@@ -1,5 +1,5 @@
 #Write a program that takes a number as input and calculates the sum of its digits
-num = int(input("Enter a Number: "))
+num = int(input("Enter a Number:"))
 
 sum_digits = 0
 while num > 0:
